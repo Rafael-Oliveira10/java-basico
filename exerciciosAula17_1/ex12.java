7111a64e2efa3d24f7e79bcaa2638bd179af33c8
Rafael Oliveira <rafael.dev42@gmail.com>
2024-03-09 19:02:14 -0300

    Exercicio da tabuada

---
+++ exerciciosAula17_1/ex12.java
@@ -0,0 +1,20 @@
+package exerciciosAula17_1;
+
+import java.util.Scanner;
+
+public class ex12 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Insira um numero da tabuada");
+        int num = scan.nextInt();
+
+        System.out.println("Tabuada de " + num + ": ");
+
+        for (int i = 0; i <= 19; i++) {
+
+            System.out.println(num + " X " + i + " = " + (num * i));
+        }
+
+    }
+}
