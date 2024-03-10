b9b2386938d5d30162958720bca5cb3d56f2a6ef
Rafael Oliveira <rafael.dev42@gmail.com>
2024-03-10 19:47:07 -0300

    POO: Conhecendo as classes

---
+++ aula22/Carro.java
@@ -0,0 +1,11 @@
+package aula22;
+// Introdução a POO
+public class Carro {
+
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+
+}
