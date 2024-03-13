b0f8e96c2d2daf64c0a8bf51f6861709014ad1dd
Rafael Oliveira <rafael.dev42@gmail.com>
2024-03-12 21:52:31 -0300

    Reforcando o aprendizado em criaÃÃação de classes em java

--- exerciciosAula24/Livro.java
+++ exerciciosAula24/Livro.java
@@ -1,2 +1,12 @@
-package exerciciosAula24;public class Livro {
+package exerciciosAula24;
+
+public class Livro {
+
+    String titulo;
+    String autor;
+    String editora;
+    int edicao;
+    int qtdPaginas;
+    long isbn;
+
 }
--- exerciciosAula24/Livro01.java
+++ exerciciosAula24/Livro01.java
@@ -1,2 +1,16 @@
-package exerciciosAula24;public class Livro01 {
+package exerciciosAula24;
+
+public class Livro01 {
+    //declarando a classe criada no arquivo livro.java.
+    public static void main(String[] args) {
+        Livro livro = new Livro();
+
+        livro.titulo = "Java";
+        livro.autor = "Deitel's Brothers";
+        livro.editora = "Pearson";
+        livro.edicao = 10;
+        livro.qtdPaginas = 968;
+        livro.isbn = 854300479;
+
+    }
 }
